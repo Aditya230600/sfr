08226ef3452291a0adcfb9b2412ba3b0f48b86cf
yasasvi-m <yasasvim@backflipt.com>
2024-12-20 20:11:54 +0530

    HomePage
    After logging into SFR user is redirected to Routing page, this file contains the check for routing page and navigating to Users page.

--- selenium_sfr/src/main/java/sfr/pages/HomePage.java
+++ selenium_sfr/src/main/java/sfr/pages/HomePage.java
@@ -1,7 +1,41 @@
 package sfr.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import sfr.base.TestBase;
 
 public class HomePage extends TestBase{
+	
+	// Page Factory - object repository
+		@FindBy(xpath = "//span[contains(text()='Users')]")
+		WebElement usersTab;
 
+		@FindBy(xpath = "//button[@id = 'Add User']")
+		WebElement addUserBtn;
+
+		@FindBy(xpath = "//h1[@id = 'Add User']")
+		WebElement addUserHeading;
+
+		public HomePage() {
+			PageFactory.initElements(driver, this);
+		}
+
+		// Actions
+		public String validateHomePageTitle() {
+			return driver.getTitle();
+		}
+
+		public String validateUsersPageTitle() {
+			usersTab.click();
+			return driver.getTitle();
+		}
+
+		public UserAddPage navigateToUserAdd() {
+			addUserBtn.click();
+			addUserHeading.click();
+			return new UserAddPage();
+		}
+	
 }
