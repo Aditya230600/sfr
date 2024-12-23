039e1cb33b085eab3f1af83a0110e859068c54f2
Aditya230600 <adityal@backflipt.com>
2024-12-23 16:05:30 +0530

    configuration page
    code upadte

--- selenium_sfr/src/main/java/sfr/pages/ConfigurationPage.java
+++ selenium_sfr/src/main/java/sfr/pages/ConfigurationPage.java
@@ -1,5 +1,31 @@
 package sfr.pages;
 
-public class ConfigurationPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import sfr.base.TestBase;
 
+public class ConfigurationPage extends TestBase {
+
+    @FindBy(xpath = "//*[@id='Configuration']")
+    WebElement configurationLink;
+    
+    @FindBy(xpath = "//*[@id='Additional Settings']")
+    WebElement additionalSettingsLink;
+
+    public ConfigurationPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickOnConfiguration() {
+        configurationLink.click();
+    }
+
+    public void clickOnAdditionalSettings() {
+        additionalSettingsLink.click();
+    }
+
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
 }
--- selenium_sfr/src/test/java/sfr/testcases/ConfigurationTest.java
+++ selenium_sfr/src/test/java/sfr/testcases/ConfigurationTest.java
@@ -1,5 +1,45 @@
 package sfr.testcases;
 
-public class ConfigurationTest {
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import sfr.base.TestBase;
+import sfr.pages.ConfigurationPage;
+import sfr.pages.HomePage;
+import sfr.pages.LoginPage;
+
+public class ConfigurationTest extends TestBase {
+    LoginPage loginPage;
+    HomePage homePage;
+    ConfigurationPage configurationPage;
+
+    public ConfigurationTest() {
+        super();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        loginPage = new LoginPage();
+        homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+        configurationPage = new ConfigurationPage();
+    }
+
+    @Test(priority = 1)
+    public void verifyAdditionalSettingsPageUrl() throws InterruptedException {
+        configurationPage.clickOnConfiguration();
+        Thread.sleep(2000);
+        configurationPage.clickOnAdditionalSettings();
+        Thread.sleep(2000);
+        String currentUrl = configurationPage.getCurrentUrl();
+        String expectedUrl = "https://qa-nyl-sfr.xeninc.us/#it-configuration---additional?otherSettings=true";
+        Assert.assertEquals(currentUrl, expectedUrl, "URL did not match the expected URL");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
