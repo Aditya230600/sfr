f1c4c12c24fa0268d07846753758965b642f91f6
Gvikas08 <vikasg@backflipt.com>
2024-12-24 15:02:38 +0530

    Testing commit
    Commit by Vikas

--- selenium_sfr/src/main/java/sfr/pages/Commit.java
+++ selenium_sfr/src/main/java/sfr/pages/Commit.java
@@ -3,7 +3,13 @@ package sfr.pages;
 public class Commit {
 	//This is a test from yasasvi-m.
 	//Git commit and push check.
+<<<<<<< Updated upstream
 // testing branch 
 // this is to test the ruleset for branch
+=======
+// testing branch
+	
+	//Testing github commit by Vikas
+>>>>>>> Stashed changes
 }
 
